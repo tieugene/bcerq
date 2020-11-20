76ef4f222941bcd5e280a68bee53460bb5c1647e
Eugene <ti.eugene@gmail.com>
2020-11-20 23:07:02 +0300

    little fix

--- misc/convert/psql/i.sql
+++ misc/convert/psql/i.sql
@@ -2,3 +2,4 @@ CREATE INDEX IF NOT EXISTS idx_txo_addr ON txo (a_id);
 CREATE INDEX IF NOT EXISTS idx_txo_date0 ON txo (date0);
 CREATE INDEX IF NOT EXISTS idx_txo_date1 ON txo (date1);
 CREATE INDEX IF NOT EXISTS idx_txo_satoshi ON txo (satoshi);
+VACUUM ANALYZE txo;
