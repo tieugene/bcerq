0c20977deb529dcd936371653421cb7401882bdd
Eugene <ti.eugene@gmail.com>
2021-10-01 18:09:55 +0300

    contrib/aio.sh prerelease

--- contrib/aio.sh
+++ contrib/aio.sh
@@ -12,45 +12,53 @@ prelog() {
     echo -n "$(date +"%y-%m-%d %H:%M:%S"): $1" | tee -a "$ERRFILE"
 }
 
+postlog() {
+    echo "$1" | tee -a "$ERRFILE"
+}
+
 log() {
     echo "$(date +"%y-%m-%d %H:%M:%S"): $1" | tee -a "$ERRFILE"
 }
 
 chk_svc() {
     # check service is active
-    prelog "Check $1... "
-    systemctl is-active --quiet $1
+    prelog "Check $1..."
+    systemctl is-active --quiet "$1"
     retcode=$?
-    if $retcode; then log "Err"; else log "OK"; fi
+    if [ $retcode -eq 0 ]; then postlog "works"; else postlog "stopped"; fi
     return $retcode
 }
 
 start_svc() {
     # try to start service
     prelog "Start $1..."
-    sudo systemctl start $1
+    sudo systemctl start "$1"
     retcode=$?
-    if $retcode; then log "Err"; else log "OK"; fi
+    if [ $retcode -eq 0 ]; then postlog "OK"; else postlog "Fail"; fi
     return $retcode
 }
 
 stop_svc() {
     # try to stop service
-    log "Stoping $1 service"
-    systemctl is-active --quiet $1 && sudo systemctl stop $1
+    prelog "Start $1..."
+    sudo systemctl stop "$1"
+    retcode=$?
+    if [ $retcode -eq 0 ]; then postlog "OK"; else postlog "Fail"; fi
+    return $retcode
 }
 
 process_bk() {
     # Process one block no $1
-    bitcoin-cli getblock $(bitcoin-cli getblockhash $1) 0 | tee >(zstd > $HEXDIR/$1.hex.zst) | \
-    bce2 -v1 -c -o -f "$1" -n 1 2>$LOGDIR/$1.log | tee >(zstd > $TXTDIR/$1.txt.zst) | \
-    $TXT2SQL | tee >(zstd > $SQLDIR/$1.sql.zst) | \
+    bitcoin-cli getblock $(bitcoin-cli getblockhash "$1") 0 | tee >(zstd > "$HEXDIR/$1.hex.zst") | \
+    bce2 -v1 -c -o -f "$1" -n 1 2>"$LOGDIR/$1.log" | tee >(zstd > "$TXTDIR/$1.txt.zst") | \
+    $TXT2SQL | tee >(zstd > "$SQLDIR/$1.sql.zst") | \
     echo "BEGIN; $(cat) COMMIT;" | psql -q "$PGBASE" "$PGLOGIN"
 }
 
 xload() {
-    log "Reload TXO"
-    $BCEDB unidx x && $BCEDB trunc x && $BCEDB xload x && (log "Index TXO"; $BCEDB idx x)
+    prelog "Reload TXO..."
+    $BCEDB unidx x && $BCEDB trunc x && $BCEDB xload x && (postlog "OK"; prelog "Index TXO..."; $BCEDB idx x)
+    postlog "OK"
 }
 
 :>"$ERRFILE"
@@ -58,24 +66,26 @@ log "== Start =="
 # 0. Prepare
 # - bce2
 BK_KV=$(bce2 -i | grep ^Chk_bk | gawk '{print $2}')
-[ -n "$BK_KV" ] && (log "Cannot ask bce2"; exit 1)
+[ -z "$BK_KV" ] && (log "Cannot ask bce2"; exit 1)
 # - bitcoin
-# FIXME: chk btcd started
 chk_svc bitcoin && SVC_BTC="1"
-[ -z "$SVC_BTC" ] && (start_svc bitcoin || (log "oops"; exit 1))
-log "The end"
-exit
+if [ -z "$SVC_BTC" ]; then
+  start_svc bitcoin || exit 1
+  prelog "Wait..."
+  sleep 10
+  postlog "OK"
+fi
 # - blockchain
 BK_BTC=$(bitcoin-cli getblockcount)
 [ -z "$BK_BTC" ] && (log "Cannot ask btcd"; exit 1)
 if [ "$BK_KV" -lt "$BK_BTC" ]; then
-  if [ -z "$BK_2ADD" ]; then let "BK_MAX=$BK_KV+$BK_2ADD-1"; else let "BK_MAX=$BK_BTC-1"; fi
-  log "Updating $BK_KV ... $BK_MAX"
-  # FIXME: chk psql started
-  chk_svc postgresql && SVC_SQL="1" || start_svc postgresql || exit 1
-  for i in $(seq "$BK_KV" "$BK_MAX"); do
-    process_bk $i
-  done
+  if [ -n "$BK_2ADD" ]; then let "BK_MAX=$BK_KV+$BK_2ADD-1"; else let "BK_MAX=$BK_BTC-1"; fi
+  log "Updating $BK_KV ... $BK_MAX required"
+  chk_svc postgresql && SVC_SQL="1"
+  if [ -z "$SVC_SQL" ]; then start_svc postgresql || exit 1; fi
+  prelog "Updating..."
+  for i in $(seq "$BK_KV" "$BK_MAX"); do process_bk "$i"; done
+  postlog "OK"
   if [ -n "$1" ]; then
     stop_svc bitcoin
     xload
@@ -87,4 +97,4 @@ if [ "$BK_KV" -lt "$BK_BTC" ]; then
 fi
 [ -z "$SVC_BTC" ] && stop_svc bitcoin
 log "== End =="
-cat "$ERRFILE" | mail -s "BTC update" "$MAILTO"
+mail -s "BTC update" "$MAILTO" < "$ERRFILE"
