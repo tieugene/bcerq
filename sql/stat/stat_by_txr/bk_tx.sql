536ce4233a57c0ed44cf3d67d73592d808a2f911
Eugene <ti.eugene@gmail.com>
2022-04-27 20:43:10 +0300

    sql/stat cleanup

--- sql/stat/stat_by_txr/bk_tx.sql
+++
@@ -1,8 +0,0 @@
--- bk_tx: list of bk tx
--- 0"
-SELECT
-  b_id,
-  MIN(id) AS tx0,
-  MAX(id) AS tx1
-FROM tx
-GROUP BY b_id;
--- sql/stat/stat_by_txr/txr_stat_bk_bad1.sql
+++
@@ -1,7 +0,0 @@
--- too long
-SELECT
-    COUNT(*) FILTER (WHERE (tx_o.b_id = 450000) AND (tx_i.b_id = 450000)) AS lo_num
-FROM vout
-INNER JOIN tx AS tx_o ON vout.t_id = tx_o.id
-INNER JOIN tx AS tx_i ON vout.t_id_in = tx_i.id
-WHERE tx_o.b_id = 450000 OR tx_i.b_id = 450000;
