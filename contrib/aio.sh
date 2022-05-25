4c55382ec173b0590ce4d969138f30dbeb15713b
Eugene <ti.eugene@gmail.com>
2022-05-25 19:50:53 +0300

    aio: options implemented (draft)

--- contrib/aio.sh
+++ contrib/aio.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
-# Direct update RDB from bitcoind
+# Daily bceX maintain
 # configs: /etc/bce/aio.cfg, ~/.aio.cfg
 # 'journalctl -t AIO' for syslog monitoring
-CFG_NAME="aio.conf"
-[ -f "/etc/bce/$CFG_NAME" ] && source "/etc/bce/$CFG_NAME"
-[ -f "$HOME/.$CFG_NAME" ] && source "$HOME/.$CFG_NAME"
-[ -z "$BINDIR" ] && { echo "No config found"; exit 1; }
-TXT2SQL="$BINDIR/txt2sql.py"
-ERRFILE="$ERRDIR/$(date +"%y%m%d%H%M%S").log"
-PRELOG=""
+
+HELP="Usage: $0 [-d] [-s] [-t] [-q]
+Options (use at least one):
+  -d:  data sync (bitcoin vs SQL DB)
+  -s:  [re]calc stat
+  -t:  [re]calc tail for last 3 months + 1 day
+  -q:  [re]calc Q1A for previous day"
+# PRELOG=""
+
+help() {
+  echo "$HELP"
+  exit
+}
+
+cli() {  # handle CLI
+  while getopts dstq opt
+  do
+    case "${opt}" in
+      d) DO_DATA=1;;
+      s) DO_STAT=1;;
+      t) DO_TAIL=1;;
+      q) DO_Q1A=1;;
+      *) help;;
+    esac
+  done
+  shift $((OPTIND-1))
+  [ -z "$DO_DATA" ] && [ -z "$DO_STAT" ] && [ -z "$DO_TAIL" ] && [ -z "$DO_Q1A" ] && help
+}
+
+load_cfg() {
+  CFG_NAME="$(basename "$0" .sh).conf"
+  # shellcheck disable=SC1090
+  [ -f "/etc/bce/$CFG_NAME" ] && source "/etc/bce/$CFG_NAME"
+  # shellcheck disable=SC1090
+  [ -f "$HOME/.$CFG_NAME" ] && source "$HOME/.$CFG_NAME"
+  [ -z "$BINDIR" ] && { echo "No config found"; exit 1; }
+  TXT2SQL="$BINDIR/txt2sql.py"
+  ERRFILE="$ERRDIR/$(date +"%y%m%d%H%M%S").log"
+}
 
 prelog() {
   PRELOG=$1
@@ -25,8 +57,7 @@ log() {
   logger -t AIO "$1"
 }
 
-chk_svc() {
-  # check service is active
+chk_svc() {  # check service is active
   prelog "Check $1..."
   systemctl is-active --quiet "$1"
   retcode=$?
@@ -34,40 +65,58 @@ chk_svc() {
   return $retcode
 }
 
-process_bk() {
-    # Process one block no $1
-    bitcoin-cli getblock $(bitcoin-cli getblockhash "$1") 0 | tee >(zstd > "$HEXDIR/$1.hex.zst") | \
-    bce2 -v1 -c -o -f "$1" -n 1 2>"$LOGDIR/$1.log" | tee >(zstd > "$TXTDIR/$1.txt.zst") | \
-    $TXT2SQL | tee >(zstd > "$SQLDIR/$1.sql.zst") | \
-    echo "BEGIN; $(cat) COMMIT;" | psql -q "$PGBASE" "$PGLOGIN"
+process_bk() {  # Process one block no $1
+  # shellcheck disable=SC2046
+  bitcoin-cli getblock $(bitcoin-cli getblockhash "$1") 0 | tee >(zstd > "$HEXDIR/$1.hex.zst") | \
+  bce2 -v1 -c -o -f "$1" -n 1 2>"$LOGDIR/$1.log" | tee >(zstd > "$TXTDIR/$1.txt.zst") | \
+  $TXT2SQL | tee >(zstd > "$SQLDIR/$1.sql.zst") | \
+  echo "BEGIN; $(cat) COMMIT;" | psql -q "$PGBASE" "$PGLOGIN"
 }
 
-:>"$ERRFILE"
-log "== Start =="
-# 0. Prepare
-# - bce2
-BK_KV=$(bce2 -i | grep ^Chk_bk | gawk '{print $2}')
-[ -z "$BK_KV" ] && { log "Cannot ask bce2"; exit 1; }
-# - bitcoind
-chk_svc bitcoin || exit 1
-# - postgresql
-chk_svc postgresql || exit 1
-# - blockchain
-BK_BTC=$(bitcoin-cli getblockcount)
-[ -z "$BK_BTC" ] && { log "Cannot ask btcd"; exit 1; }
-# 1. main
-if [ "$BK_KV" -lt "$BK_BTC" ]; then
-  if [ -n "$BK_2ADD" ]; then BK_MAX=$((BK_KV+BK_2ADD-1)); else BK_MAX=$((BK_BTC-1)); fi
-  log "Updating $BK_KV ... $BK_MAX required"
-  for i in $(seq "$BK_KV" "$BK_MAX"); do process_bk "$i"; done
+do_data() {
+  # - bce2
+  BK_KV=$(bce2 -i | grep ^Chk_bk | gawk '{print $2}')
+  [ -z "$BK_KV" ] && { log "Cannot ask bce2"; exit 1; }
+  # - bitcoind
+  chk_svc bitcoin || exit 1
+  # - blockchain
+  BK_BTC=$(bitcoin-cli getblockcount)
+  [ -z "$BK_BTC" ] && { log "Cannot ask btcd"; exit 1; }
+  # 1. main
+  if [ "$BK_KV" -lt "$BK_BTC" ]; then
+    if [ -n "$BK_2ADD" ]; then BK_MAX=$((BK_KV+BK_2ADD-1)); else BK_MAX=$((BK_BTC-1)); fi
+    log "Updating $BK_KV ... $BK_MAX required"
+    for i in $(seq "$BK_KV" "$BK_MAX"); do process_bk "$i"; done
+  fi
+}
+
+do_stat() {
   log "Stat..."
+  # shellcheck disable=SC2046
   cat $(dirname "$0")/../sql/stat/{u_stat_bk.sql,u_stat_bk_inc.sql,u_stat_date.sql,u_stat_date_inc.sql} | psql -q -v ON_ERROR_STOP=on "$PGBASE" "$PGLOGIN"
+}
+
+do_tail() {
   TAIL_FROM=$(date -d "-3 month -1 day" +"%Y-%m-%d")
   log "Tail (from $TAIL_FROM)"
   psql -q -c "CALL _tail_refill('$TAIL_FROM')" "$PGBASE" "$PGLOGIN"
+}
+
+do_q1a() {
   Q1A_DATE=$(date -d "-1 day" +"%Y-%m-%d")
   log "Q1A ($Q1A_DATE)"
   psql -q -c "CALL _daily('$Q1A_DATE')" "$PGBASE" "$PGLOGIN"
-fi
+}
+
+cli
+load_cfg
+:>"$ERRFILE"
+log "== Start =="
+chk_svc postgresql || exit 1
+[ "$DO_DATA" ] || do_data
+[ "$DO_STAT" ] || do_stat
+[ "$DO_TAIL" ] || do_tail
+[ "$DO_Q1A" ] || do_q1a
 log "== End =="
+# shellcheck disable=SC2153
 mail -S "from=$MAILFROM" -s "aio" "$MAILTO" < "$ERRFILE"
